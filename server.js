b0ebd9124576070a55c5149376c4cf5d6d161b07
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-06 00:32:12 -0400

    setting mongo in heroku deployment

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://root1:rootroot1@ds049130.mlab.com:49130/heroku_hrgqbvfc", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
