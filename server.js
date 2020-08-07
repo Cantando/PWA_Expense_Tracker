97227e5b28e08fc0dd699e6cbefbbbc136d6b41c
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-06 20:48:45 -0400

    added profile

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.port || 8080;
+const PORT= process.env.port || 8080;
 
-const app = express();
+const app= express();
 
 app.use(logger("dev"));
 
